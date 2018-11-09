310d4789604c15ea7f6fbf0558f783592edec91e
Tirapa <44288014+Tirapa@users.noreply.github.com>
2018-11-09 16:29:38 +0700

    Update app/src/main/java/com/myweb/lab8/MainActivity.java

--- app/src/main/java/com/myweb/lab8/MainActivity.java
+++ app/src/main/java/com/myweb/lab8/MainActivity.java
@@ -2,8 +2,35 @@ package com.myweb.lab8;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText txtStdid;
+    private EditText txtStdName;
+    private EditText txtStdTel;
+    private EditText txtStdEmail;
+    private EditText editStdId = null;
+    private EditText editStdName = null;
+    private EditText editStdTel = null;
+    private EditText editStdEmail = null;
+    private Button btnSave;
+    private Button btnShow;
+    private ListView dataView;
+    private ListView clickView;
+    private MySQLConnect mySQLConnect;
+    private List<String> items;
+    private ArrayAdapter<String> adt;
+    private String dataStdId = null;
+    private String dataStdName = null;
+    private String dataStdTel = null;
+    private String dataStdEmail = null;
+    private View promptView = null
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
