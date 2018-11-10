d02153714303b264150a7e341fc858a402a7107e
Tirapa <44288014+Tirapa@users.noreply.github.com>
2018-11-10 14:10:34 +0700

    commit

--- app/src/main/java/com/myweb/lab8/MainActivity.java
+++ app/src/main/java/com/myweb/lab8/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init ();
+        init();
         update();
 
         clickView = findViewById(R.id.dataView);
@@ -47,23 +47,23 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int pos, long l) {
-                String selectFromList = (clickView.getItemAtPosition(pos).toString()+"");
+                String selectFromList = (clickView.getItemAtPosition(pos).toString() + "");
                 int indextel;
                 String caltel = "";
-                dataStdId = selectFromList.substring(0,11);
+                dataStdId = selectFromList.substring(0, 11);
                 caltel = selectFromList.substring(12);
                 indextel = caltel.indexOf("0");
-                dataStdName = caltel.substring(0,indextel-1);
-                dataStdTel = caltel.substring(indextel,indextel+10);
-                dataStdEmail = caltel.substring(indextel+11);
+                dataStdName = caltel.substring(0, indextel - 1);
+                dataStdTel = caltel.substring(indextel, indextel + 10);
+                dataStdEmail = caltel.substring(indextel + 11);
                 return true;
             }
         });
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                mySQLConnect.sentData(txtStdid.getText().toString(),txtStdName.getText().toString(),txtStdTel.getText().toString(),txtStdEmail.getText().toString());
-                items.add(txtStdid.getText().toString()+"\n"+txtStdName.getText().toString()+"\n"+txtStdTel.getText().toString()+"\n"+txtStdEmail.getText().toString());
+            public void onClick(View v) {
+                mySQLConnect.sentData(txtStdid.getText().toString(), txtStdName.getText().toString(), txtStdTel.getText().toString(), txtStdEmail.getText().toString());
+                items.add(txtStdid.getText().toString() + "\n" + txtStdName.getText().toString() + "\n" + txtStdTel.getText().toString() + "\n" + txtStdEmail.getText().toString());
                 adt.notifyDataSetChanged();
                 txtStdid.setText("");
                 txtStdName.setText("");
@@ -82,3 +82,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
+
+
+
